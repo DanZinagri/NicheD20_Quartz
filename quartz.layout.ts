0138092f0e6fdc3d86ecca90df6b5459290dcf66
DanZinagri <98767002+DanZinagri@users.noreply.github.com>
2024-09-09 07:02:07 -0500

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -3,8 +3,8 @@ import * as Component from "./quartz/components"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
-  head: Component.Head(),
-  header: [Component.Darkmode(),],
+  head: [Component.Head(),Component.Darkmode(),]
+  header: [],
   afterBody: [],
   footer: Component.Footer({
     links: {
